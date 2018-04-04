63e9e4dc4a1f01cb64b13437df077a8bf0086591
hjin36 <37239988+hjin36@users.noreply.github.com>
2018-04-03 22:50:40 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+#getting the packages
+
 if (!"reshape2" %in% installed.packages()) {
   install.packages("reshape2")
 }
@@ -13,6 +15,7 @@ library(reshape2)
 library(dplyr)
 library(tidyr)
 
+#download the file
 if(!dir.exists("./datafile")){
   dir.create("datafile")
 }
@@ -23,6 +26,7 @@ unzip("Dataset.zip")
 setwd("UCI HAR Dataset")
 
 
+#Merges the training and the test sets to create one data set.
 
 train_subject <- read.table("./train/subject_train.txt",sep="\t")
 train_set <- read.table("./train/X_train.txt",header = FALSE)
@@ -36,26 +40,30 @@ test_label <- read.table("./test/y_test.txt",header = FALSE)
 
 test <- data.frame(test_subject,test_label,test_set)
 
+feature <- read.table("features.txt")
+
 all <- rbind(test,train)
+
+# Uses descriptive activity names to name the activities in the data set
 colnames(all)[1:2] <- c("subject","label")
+colnames(all)[3:563] <- as.character(feature[,2])
 
-all2 <- gather(all,activity,value,-c(subject,label))
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+mean.sd <- grep("-mean\\(\\)|-std\\(\\)", feature[, 2])
+all.mean.sd <- all[,mean.sd]
 
-all2 <- all2[,c(1,2,4)]
-
-summary <- all2 %>%
-  group_by(subject,label) %>%
-  summarise(average = mean(value),standard_deviation = sd(value))
-
-
+# Uses descriptive activity names to name the activities in the data set
 label_text <- read.table("activity_labels.txt")
 
 for (i in c(1,2,3,4,5,6)){
-  summary$label[which(summary$label==i)] <- as.character(label_text$V2[which(label_text$V1 == i)])
+  all.mean.sd$label[which(all.mean.sd$label==i)] <- as.character(label_text$V2[which(label_text$V1 == i)])
 }
 
-tidyset <- select(summary,subject,label,average)
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+all$label <- factor(all$label, levels = label_text[,1], labels = label_text[,2])
+all$subject <- as.factor(all$subject)
 
-summary
-tidyset
+all.melted <- melt(all, id = c("subject", "label"))
+all.mean <- dcast(all.melted, subject + label ~ variable, mean)
 
+write.table(all.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
